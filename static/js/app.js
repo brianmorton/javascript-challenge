cf10db1fcb30a58df67a14f38bbc6400e44b50d8
Brian Morton <bmorton925@gmail.com>
2021-04-06 11:04:36 -0400

    initial table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,36 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var filtbutton = d3.select(".button");
+
+
+//d3 function append tr for object
+ data.forEach(function(ufoReport) {
+   var row = tbody.append("tr");
+   Object.entries(ufoReport).forEach(function([key, value]) {
+//Append to cell in the row for each value with td per entry
+    var cell = row.append("td");
+//append text to cell
+    cell.text(value);
+   });
+ });
+
+
+
+//button on click function
+
+
+//filtbutton.on("click", function() {
+//console.log('test button click')
+//}
+
+
+//pull from text field
+
+
+//run filter statement
+
+
+
+//rerun cell update function
