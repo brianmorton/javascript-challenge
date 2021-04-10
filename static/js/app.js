bfc755c2946fbd884cbdbbfcda366f213229cd98
Brian Morton <bmorton925@gmail.com>
2021-04-10 09:55:03 -0400

    final push

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,19 @@
 // from data.js
 var tableData = data;
 //empty array for filtering
-var filteredData = [];
+//var filteredData = [];
 
 //select html 
 var tbody = d3.select("tbody");
 //select button and form
-var filtbutton = d3.select("#filter-btn");
+var filtbutton = d3.selectAll("#filter-btn");
 var form = d3.select("#form");
 
-
-//d3 function append tr for object
-tableData.forEach(function(ufoReport) {
+//wrap in function argurement 
+function buildTable(data){
+  tbody.html("");
+  //d3 foreach append tr for object
+data.forEach(function(ufoReport) {
    var row = tbody.append("tr");
    Object.entries(ufoReport).forEach(function([key, value]) {
 //Append to cell in the row for each value with td per entry
@@ -20,7 +22,7 @@ tableData.forEach(function(ufoReport) {
     cell.text(value);
    });
  });
-
+};
 
 //button on click function
 filtbutton.on("click", textpull);
@@ -31,29 +33,22 @@ function testfunc() {
   d3.event.preventDefault();
   var inputElement = d3.select("#datetime");
   var inputdate = inputElement.property("value");
+  console.log("input data: ", inputdate);
   return inputdate;
 }
 
-//pull from text field
+//pull from text field function
 function textpull() 
 {
-  d3.event.preventDefault();
+  //d3.event.preventDefault();
   var inputElement = d3.select("#datetime");
   var inputdate = inputElement.property("value");
+  var filteredData = tableData;
   //run filter statement
-  filteredData = tableData.filter(table => table.datetime === inputdate);
-  return filteredData;
-}
+  filteredData = filteredData.filter(table => table.datetime === inputdate);
+  console.log(filteredData);
+  buildTable(filteredData);
+};
 
-//getting error: blank array? 
-console.log(filteredData);
+buildTable(tableData);
 
-//rerun cell update function with refactored d3 function
-
-filteredData.forEach(function(ufoReport) {
- var row = tbody.append("tr");
- Object.entries(ufoReport).forEach(function([key, value]) {
-  var cell = row.append("td");
-  cell.text(value);
- });
-});
